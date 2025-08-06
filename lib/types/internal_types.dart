1ce57d1d0e14f8ad5dff42bd04181c57df7ac1e1
Madeline Watts <madeline.chloe429@gmail.com>
2025-08-06 09:43:14 -0600

    user and doc types

--- lib/types/internal_types.dart
+++ lib/types/internal_types.dart
@@ -14,3 +14,35 @@ class FoodIdResponseData {
         name: json['name'], time: json['time'], notes: json['notes']);
   }
 }
+
+class Document {
+  final String id;
+  final int createdAt;
+  final int updatedAt;
+  int? deletedAt;
+
+  Document({
+    required this.id,
+    required this.createdAt,
+    required this.updatedAt,
+    this.deletedAt,
+  });
+}
+
+class User extends Document {
+  final String anonId;
+  final int version;
+  final int userSince;
+  final List<String> symptoms;
+
+  User({
+    required super.id,
+    required super.createdAt,
+    required super.updatedAt,
+    required super.deletedAt,
+    required this.anonId,
+    required this.version,
+    required this.userSince,
+    required this.symptoms,
+  });
+}
