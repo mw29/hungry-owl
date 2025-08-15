27a85c7460137efee89c270f1a9ae09d17483bea
Madeline Watts <madeline.chloe429@gmail.com>
2025-08-15 13:08:22 -0600

    adjust symptom inputfield

--- lib/screens/symptom_settings.dart
+++ lib/screens/symptom_settings.dart
@@ -38,6 +38,7 @@ class _SymptomSettings extends ConsumerState<SymptomSettings> {
       });
       updateUser(updatedData: {'symptoms': newSymptoms});
     }
+        FocusScope.of(context).unfocus();
   }
 
   void _removeSymptom(String symptom) {
@@ -65,72 +66,79 @@ class _SymptomSettings extends ConsumerState<SymptomSettings> {
     });
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            const Text(
-              'What are your symptoms?',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'These symptoms will be used in ingredient analysis when you scan or add a food.',
-              style: TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _symptomController,
-                    decoration: const InputDecoration(
-                      labelText: 'Symptom',
-                      border: OutlineInputBorder(),
-                      hintText: 'Enter a symptom',
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              const Text(
+                'What are your symptoms?',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'These symptoms will be used in ingredient analysis when you scan or add a food.',
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      maxLines: null,
+                      minLines: 1,
+                      controller: _symptomController,
+                      decoration: const InputDecoration(
+                        labelText: 'Symptom',
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter a symptom',
+                      ),
+                      onSubmitted: (_) => _addSymptom(),
                     ),
-                    onSubmitted: (_) => _addSymptom(),
                   ),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: _addSymptom,
-                  child: const Text('Add'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Wrap(
-              spacing: 8.0,
-              runSpacing: 4.0,
-              children: _symptoms.map((symptom) {
-                return Material(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Flexible(
-                          child: Text(
-                            symptom,
-                            softWrap: true,
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: _addSymptom,
+                    child: const Text('Add'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Wrap(
+                spacing: 8.0,
+                runSpacing: 4.0,
+                children: _symptoms.map((symptom) {
+                  return Material(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 15),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Flexible(
+                            child: Text(
+                              symptom,
+                              softWrap: true,
+                            ),
                           ),
-                        ),
-                        const SizedBox(width: 8),
-                        GestureDetector(
-                          onTap: () => _removeSymptom(symptom),
-                          child: const Icon(Icons.close, size: 18),
-                        ),
-                      ],
+                          const SizedBox(width: 8),
+                          GestureDetector(
+                            onTap: () => _removeSymptom(symptom),
+                            child: const Icon(Icons.close, size: 18),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              }).toList(),
-            ),
-          ],
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
         ),
       ),
     );
